2d797cebc02f92b8cdf27a4eb79d2bfaca98a27a
luqman <luqskywalker@gmail.com>
2016-11-16 11:43:55 +0800

    C2:Add testFragment

---
+++ app/src/main/java/xyz/sfero/testgetcontact/testFragment.java
@@ -0,0 +1,21 @@
+package xyz.sfero.testgetcontact;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by luqskywalker on 16-Nov-2016.
+ */
+
+public class testFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
